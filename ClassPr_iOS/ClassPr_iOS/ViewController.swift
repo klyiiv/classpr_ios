e84e7957dd38fa03e53cca3d07c62710619cbe51
danya <d4k07@yandex.ru>
2023-10-16 20:02:40 +0300

    added BeerGTO

--- ClassPr_iOS/ClassPr_iOS/ViewController.swift
+++ ClassPr_iOS/ClassPr_iOS/ViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.systemMint
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard
+                let data = data,
+                error == nil
+            else {
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let model = try! decoder.decode([BeerDTO].self, from: data)
+            print(model)
+        }).resume()
+        }
     }
 
-
+struct BeerDTO: Decodable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let imageUrl: URL
+    // go deep
 }
-
